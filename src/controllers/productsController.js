f218dd2dfbc95af781ff2377e53b8ee9ca1b6199
LeonelAnchaba <leonelanchaba@gmail.com>
2023-12-14 19:54:49 -0300

    refactoricé y modularicé, hasta ahora lo que son los metodos de lectura y escritura de los json, y lo apliqué en el home, en la sección productos, en el detalle y en la edición de productos. Falta aplicarlo en la creación de productos y en la eliminación de los mismos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,11 +3,8 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const getJson = ()=>{
-	const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-	const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-	return products
-}
+const {setJson,getJson} = require("../utility/jsonMethod");
+
 
 
 
@@ -16,13 +13,13 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		res.render('products',{products, toThousand})
+		res.render('products',{products:getJson("productsDataBase"), toThousand})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
 	const {id} = req.params	
-	const products = getJson()
+	const products = getJson("productsDataBase")
 	const product = products.find(producto => producto.id == id)
 
 	res.render('detail',{product, toThousand})
@@ -66,7 +63,7 @@ const controller = {
 	// Update - Form to edit
 	edit: (req, res) => {
 		 const {id} = req.params
-		 const products = getJson()
+		 const products = getJson("productsDataBase")
 		 const product = products.find(producto => producto.id == id)
 		 res.render('product-edit-form',{product})
 
@@ -74,7 +71,7 @@ const controller = {
 	// Update - Method to update
 	update: (req, res) => {
 		const {id} = req.params
-		const products = getJson()
+		const products = getJson("productsDataBase")
 		const {name, price, discount, category, description, image} = req.body
 		const nuevoArray = products.map(product => {
 			if(product.id == id){
@@ -90,9 +87,8 @@ const controller = {
 			}
 			return product
 		})
-		const json = JSON.stringify(nuevoArray)
-		fs.writeFileSync(productsFilePath, json, "utf-8")
-		res.redirect(`/products`)
+		setJson(nuevoArray, "productsDataBase")
+		res.redirect(`/products/${id}`)
 	},
 
 	// Delete - Delete one product from DB
