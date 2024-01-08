cb30ab80a9bf76687479b704ffadcc12b5f41b83
LeonelAnchaba <leonelanchaba@gmail.com>
2024-01-08 11:46:25 -0300

    Si el usuario no selecciona una imagen, se le carga una por defecto. Tiene validaciones para las extensiones unicamente

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -28,32 +28,32 @@ const controller = {
 
   // Create -  Method to store
   store: (req, res) => {
-      const file = req.file
-      const archivoJson = getJson("productsDataBase");
-      const id = archivoJson[archivoJson.length - 1].id + 1;
-     
-  
-      const { name, price, discount, category, description} = req.body;
-      // if(!file) {
-      //   const error = new Error("Por favor seleccione un archivo")
-      //   error.httpStatusCode = 400
-      //   res.send(error)
-      // }
-  
-      let newObjeto = {
-        id,
-        name,
-        price: +price,
-        discount: +discount,
-        category,
-        description,
-        image: file ? file.filename : "default-image.png",
-        
-      };
-    
-      let newArchivo = [...archivoJson, newObjeto];
-      setJson(newArchivo, "productsDataBase");
-      res.redirect("/products");
+    const file = req.file
+    const archivoJson = getJson("productsDataBase");
+    const id = archivoJson[archivoJson.length - 1].id + 1;
+
+
+    const { name, price, discount, category, description } = req.body;
+    // if(!file) {
+    //   const error = new Error("Por favor seleccione un archivo")
+    //   error.httpStatusCode = 400
+    //   res.send(error)
+    // }
+
+    let newObjeto = {
+      id,
+      name,
+      price: +price,
+      discount: +discount,
+      category,
+      description,
+      image: file ? file.filename : "default-image.png",
+
+    };
+
+    let newArchivo = [...archivoJson, newObjeto];
+    setJson(newArchivo, "productsDataBase");
+    res.redirect("/products");
   },
 
   // Update - Form to edit
@@ -68,7 +68,7 @@ const controller = {
     const file = req.file
     const { id } = req.params;
     const products = getJson("productsDataBase");
-    const { name, price, discount, category, description} = req.body;
+    const { name, price, discount, category, description } = req.body;
     const nuevoArray = products.map((product) => {
       if (product.id == id) {
         return {
@@ -83,26 +83,26 @@ const controller = {
       }
       return product;
     });
-  
+
     setJson(nuevoArray, "productsDataBase");
     res.redirect(`/products/${id}`);
   },
 
   // Delete - Delete one product from DB
   destroy: (req, res) => {
-	const {id}= req.params;
+    const { id } = req.params;
     const archivoJson = getJson("productsDataBase");
     const product = archivoJson.find(producto => producto.id == id);
 
     //con fs vamos a borrar la imagen antes de borrar el articulo, para no perder la referencia
-  
+
 
     const productosRestantes = archivoJson.filter(product => product.id != id);
     // product.images.forEach(imagen => {
     //   fs.unlink(`../../public/images/products/${imagen}`)
     // })
     fs.unlink(`./public/images/products/${product.image}`, (err) => {
-      if(err) throw err
+      if (err) throw err
       console.log("Archivo borrado")
     })
     setJson(productosRestantes, "productsDataBase");
