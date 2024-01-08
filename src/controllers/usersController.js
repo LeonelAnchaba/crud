42491af27cb717f96e07dc4de1ae791fcadbd6cb
LeonelAnchaba <leonelanchaba@gmail.com>
2024-01-07 23:54:39 -0300

    Si el formulario no tiene errores, ya carga el nuevo usuario en la base de datos. Falta resolver la parte de lo que va a hacer cuando haya errores. Tanto la lógica del controlador, como la de la vista del formulario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,32 +1,37 @@
 const { setJson, getJson } = require("../utility/jsonMethod");
 const fs = require("fs")
+const {validationResult} = require("express-validator")
 
 const usersController = {
     formRegister:(req,res)=>{
         res.render('register',{title:"Registro"})
         
     },
-    // register:(req,res) => {
-    //      console.log(req.body)
-    //      res.redirect("/")
     register:  (req, res) => {
-        const archivoJson = getJson("usersDataBase");
+        const errors = validationResult(req)
+        
+        if (errors.isEmpty()){
+            const archivoJson = getJson("usersDataBase");
            
         
-        const { name, surname, email, password} = req.body;
-          
+            const { name, surname, email, password} = req.body;
+             
+           
+                let newUser = {
+                  name,
+                  surname,
+                  email,
+                  password
+                 
+                };
+             
+                let newArchivo = [...archivoJson, newUser];
+                setJson(newArchivo, "usersDataBase");
+                res.redirect("/");
+        } else {
+            res.send(errors)
+        }
         
-            let newUser = {
-              name,
-              surname,
-              email,
-              password
-              
-            };
-          
-            let newArchivo = [...archivoJson, newUser];
-            setJson(newArchivo, "usersDataBase");
-            res.redirect("/");
         
     },
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,42 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-// const {check} = require("express-validator")
+const {check} = require("express-validator")
+// const multer = require("multer");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//     destination: (req,file,cb) => {
+//         cb(null, path.join(__dirname, "../../public/images/users"))
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null,`${Date.now()}_img_${path.extname(file.originalname)}` )
+//     }
+// })
+
+//const uploadFile = multer({ storage})
+
+
+const validateRegister = [
+    check('name')
+    .notEmpty().withMessage('Debe completar su nombre').bail()
+    .isLength({ min: 3 }).withMessage('Su nombre debe contar al menos con 3 caracteres'),
+    check("surname")
+    .notEmpty().withMessage('Debe completar su apellido').bail()
+    .isLength({ min: 3 }).withMessage('Su apellido debe contar al menos con 3 caracteres'),
+    check("email")
+    .notEmpty().withMessage("Debe ingresar su email").bail()
+    .isEmail().withMessage("El mail ingresado no es válido"),
+    check("password")
+    .notEmpty().withMessage("Debe completar su contraseña").bail()
+    .isLength({ min:8 }).withMessage("La contraseña debe tener un minimo de 8 caracteres")
+];
 
 // ************ Controller Require ************
 const {formRegister, register} = require('../controllers/usersController');
 
 router.get('/register', formRegister); 
-router.post('/register', register); 
+// uploadFile.single("image")
+router.post('/register', validateRegister, register); 
 
 module.exports = router;
